9123125464e0e5a41e807c30d15b97d1780f7d42
Dmjosus243 <dmpugondo@gmail.com>
2025-08-31 11:19:50 +0100

    Correction des codes JavaScripts du documents SCRIPTS pour eviter les erreurs

--- Codebase/SCRIPT/Html.js
+++ Codebase/SCRIPT/Html.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    //Mettre un surlignement dans le lien active 
+    // Mettre un surlignement dans le lien actif dans la navigation
     const navLinks = document.querySelectorAll('nav a');
     navLinks.forEach(link => {
-        if (window.location.pathname.endsWith(link.getAttribute('href'))) {
-            link.style.textDecoration = 'underline';
-            link.style.fontWeight = 'bold';
+        const linkPath = link.getAttribute('href');
+        if (window.location.pathname.endsWith(linkPath)) {
+            link.classList.add('active');
         }
     });
 });
--- Codebase/SCRIPT/script.js
+++ Codebase/SCRIPT/script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Mettre un surlignement dans  le lien actif dans la navigation
+    // Mettre un surlignement dans le lien actif dans la navigation
     const navLinks = document.querySelectorAll('nav a');
     navLinks.forEach(link => {
-        if (window.location.pathname.endsWith(link.getAttribute('href'))) {
-            link.style.fontWeight = 'bold';
-            link.style.textDecoration = 'underline';
+        const linkPath = link.getAttribute('href');
+        if (window.location.pathname.endsWith(linkPath)) {
+            link.classList.add('active');
         }
     });
-});7
+});
